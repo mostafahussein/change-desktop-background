4f1aff9c1f7666410d283c20aa02ee8e06995d9a
Mostafa Hussein <mostafa.hussein91@gmail.com>
2015-11-15 20:12:01 +0200

    some modifications to meet with PEP8 standards

--- background.py
+++ background.py
@@ -1,40 +1,42 @@
-'''
-Simple python script to change desktop background on startup and every 12 hours.
-'''
-import urllib
+"""
+Simple python script to change desktop background on startup and every 12 hours
+"""
 import os
 import time
+import urllib
+
 
 def get_image():
-    '''
+    """
     Get an image from Unsplash Source <https://source.unsplash.com/>
     and save it locally on the current user's home
-    '''
+    """
     img_api_url = urllib.urlopen("https://source.unsplash.com/category/nature")
-    #download the image
+    # download the image
     img_path = os.path.expanduser('~') + '/Pictures/bg.jpg'
     img_file = open(img_path, 'wb')
     img_file.write(img_api_url.read())
     img_file.close()
     return img_path
 
+
 def change_background():
-    '''
+    """
     - Set the image using command-line
     - Change background every 12 hr
-    '''
+    """
     img = get_image()
     cmd = 'gsettings set org.gnome.desktop.background picture-uri'
     os.system("{0} file://{1}".format(cmd, img))
 
     while True:
         change_background()
-        #change background every 12 hrs
+        # change background every 12 hrs
         time.sleep(43200)
 
 if __name__ == "__main__":
     try:
         change_background()
     except KeyboardInterrupt:
-        #print a message when using CTRL+C to exit
+        # print a message when using CTRL+C to exit
         print 'exit'
